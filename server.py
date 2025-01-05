7ca7e2988b2ec1cadbc4cb5e4bf0c4c276bd33c6
Balaji B <balaji.balachandar1@gmail.com>
2025-01-05 15:13:03 +0530

    Added my info

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, url_for
+import smtplib
+
+load_dotenv("C:/Python/Environmental variables/.env")
+my_mail = "sampleforpythonmail@gmail.com"
+password = os.getenv("smtp_app_password")
 
 app = Flask(__name__)
 
@@ -6,5 +13,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/',methods=["POST","GET"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data['name'])
+        # with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        #     connection.starttls()
+        #     connection.login(my_mail, password=password)
+        #     connection.sendmail(from_addr=my_mail,
+        #                         to_addrs=my_mail,
+        #                         msg=f"Subject:Message from Portfolio\n\n"
+        #                             f"Name : {data['name']}\n"
+        #                             f"Email : {data['email']}\n"
+        #                             f"Message : {data['message']}")
+    return url_for('home')
 if "__main__" == __name__:
     app.run(debug=True)
