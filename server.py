4b45b97d36bba6ce024ee8e8f4623ed6f1aa4c32
Balaji B <balaji.balachandar1@gmail.com>
2025-01-11 22:26:33 +0530

    Initial Commit

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
 import smtplib
 
 load_dotenv("C:/Python/Environmental variables/.env")
@@ -13,20 +13,23 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/downloadable_resume")
+def download():
+    return send_from_directory(directory="static",path="assets/res/resume.pdf")
+
 @app.route('/',methods=["POST","GET"])
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data['name'])
-        # with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        #     connection.starttls()
-        #     connection.login(my_mail, password=password)
-        #     connection.sendmail(from_addr=my_mail,
-        #                         to_addrs=my_mail,
-        #                         msg=f"Subject:Message from Portfolio\n\n"
-        #                             f"Name : {data['name']}\n"
-        #                             f"Email : {data['email']}\n"
-        #                             f"Message : {data['message']}")
-    return url_for('home')
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(my_mail, password=password)
+            connection.sendmail(from_addr=my_mail,
+                                to_addrs=my_mail,
+                                msg=f"Subject:Message from Portfolio\n\n"
+                                    f"Name : {data['name']}\n"
+                                    f"Email : {data['email']}\n"
+                                    f"Message : {data['message']}")
+        return redirect(url_for('home'))
 if "__main__" == __name__:
     app.run(debug=True)
